cef4200deadb272001e7dab1eec904a5b0610656
nhan502n <nhan502n@gmail.com>
2025-04-11 19:24:14 +0700

    update

---
+++ database/migrations/2025_04_11_121213_create_vouchers_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('vouchers', function (Blueprint $table) {
+            $table->id();
+            $table->string('code', 50);
+            $table->decimal('discount_value', 10, 2)->default(0);
+            $table->enum('discount_type', ['percent', 'fixed'])->default('percent');
+            $table->decimal('max_discount_value', 10, 2)->nullable();
+            $table->decimal('min_order_value', 10, 2)->default(0);
+            $table->integer('usage_limit')->nullable();
+            $table->integer('usage_count')->default(0);
+            $table->date('start_date')->nullable();
+            $table->date('end_date')->nullable();
+            $table->unsignedBigInteger('created_by');
+            $table->foreign('created_by')->references('id')->on('users')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('vouchers');
+    }
+};
