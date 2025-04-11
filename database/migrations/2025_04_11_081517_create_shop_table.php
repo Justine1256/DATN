ec353472dab5b483890c9b5d0b08bfd967cc131d
nhan502n <nhan502n@gmail.com>
2025-04-11 15:31:05 +0700

    new

---
+++ database/migrations/2025_04_11_081517_create_shop_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('shop', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 100)->unique();
+            $table->string('description', 255);
+            $table->tinyInteger('rating', 2,1);
+            $table->enum('status',['actived','locked','hidden'])->default('actived');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('shop');
+    }
+};
