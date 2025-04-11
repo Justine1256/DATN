01af29bd8f0cd0673bb82d2eda2a4dd9ca27e819
zhen108 <baotruong1082003@gmail.com>
2025-04-11 19:03:30 +0700

    Update 2025_04_11_085337_create_products_table.php

--- database/migrations/2025_04_11_085337_create_products_table.php
+++ database/migrations/2025_04_11_085337_create_products_table.php
@@ -1,27 +1,34 @@
 <?php
-
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('category_id');
+            $table->unsignedBigInteger('shop_id');
+            $table->foreign('category_id')->references('id')->on('categories')->onDelete('cascade');
+            $table->foreign('shop_id')->references('id')->on('shops')->onDelete('cascade');
             $table->string('name');
-            $table->string('image');
+            $table->text('description')->nullable();
+            $table->decimal('price', 10, 2);
+            $table->integer('stock')->default(0);
+            $table->string('image')->nullable();
+            $table->string('option1')->nullable();
+            $table->string('value1')->nullable();
+            $table->string('option2')->nullable();
+            $table->string('value2')->nullable();
+            $table->enum('status', ['Active', 'Deleted'])->default('Active');
             $table->timestamps();
+            $table->softDeletes();
+
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::dropIfExists('products');
