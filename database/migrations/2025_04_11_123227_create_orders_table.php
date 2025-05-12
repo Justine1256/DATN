71028d47a4c3c3df6f315a05c82103b50db214d7
zhen108 <baotruong1082003@gmail.com>
2025-05-13 00:12:35 +0700

    up

--- database/migrations/2025_04_11_123227_create_orders_table.php
+++ database/migrations/2025_04_11_123227_create_orders_table.php
@@ -12,10 +12,13 @@ return new class extends Migration
             $table->id();
             $table->foreignId('user_id')->constrained('users')->cascadeOnDelete();
             $table->foreignId('shop_id')->constrained('shops')->cascadeOnDelete();
+            $table->decimal('final_amount', 10, 2);
             $table->decimal('total_amount', 10, 2);
             $table->enum('payment_method', ['COD', 'Card', 'Wallet']);
             $table->enum('payment_status', ['Pending', 'Completed', 'Failed'])->default('Pending');
+            $table->string('transaction_id')->nullable();
             $table->enum('order_status', ['Pending', 'Shipped', 'Delivered', 'Canceled'])->default('Pending');
+            $table->enum('shipping_status', ['Pending', 'Shipping', 'Delivered'])->default('Pending');
             $table->text('shipping_address');
             $table->timestamps();
             $table->softDeletes();
