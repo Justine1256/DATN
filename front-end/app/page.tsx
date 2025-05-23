483aa4af3050f661ac66a3e55faa5faac97574a0
nhan502n <nhan502n@gmail.com>
2025-05-23 12:21:47 +0700

    chuyển banner từ layout sang trang chủ

--- front-end/app/page.tsx
+++ front-end/app/page.tsx
@@ -1,12 +1,24 @@
-import Head from 'next/head';
+import CategoryMenuWithBanner from "@/app/components/common/Banner";
+import Header from "@/app/components/common/Header";
+import { Geist } from "next/font/google";
 
-export default function Home() {
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+export default function HomeLayout({ children }: { children: React.ReactNode }) {
+  const headerTopHeight = 38;
+  const headerBottomHeight = 60;
+  const headerHeight = headerTopHeight + headerBottomHeight;
+
   return (
-   
-      <Head>
-       
-      </Head>
-
-  
+    <html lang="en">
+      <body className={geistSans.className}>
+        <Header />
+        <CategoryMenuWithBanner headerHeight={headerHeight} />
+        {children}
+      </body>
+    </html>
   );
 }
