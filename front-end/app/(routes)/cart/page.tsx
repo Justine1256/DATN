6e4fdc94db4f13499288f10ad0a7250971ce6791
Thùy Anh <huynhphamthuyanh.126@gmail.com>
2025-06-07 10:13:13 +0700

    Add font-size config

---
+++ front-end/app/(routes)/cart/page.tsx
@@ -0,0 +1,3 @@
+export default function CartPage() { 
+    return null; // Replace with your actual component implementation 
+    }
--- front-end/tailwind.config.js
+++ front-end/tailwind.config.js
@@ -1,5 +1,7 @@
 // tailwind.config.js
 
+import tailwindScrollbarHide from "tailwind-scrollbar-hide";
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
@@ -10,16 +12,23 @@ module.exports = {
   theme: {
     extend: {
       animation: {
-        'loading-bar': 'loading 2s ease-in-out infinite',
+        "loading-bar": "loading 2s ease-in-out infinite",
       },
       keyframes: {
         loading: {
-          '0%': { width: '0%' },
-          '50%': { width: '80%' },
-          '100%': { width: '0%' },
+          "0%": { width: "0%" },
+          "50%": { width: "80%" },
+          "100%": { width: "0%" },
         },
+      },
+      plugins: [tailwindScrollbarHide],
+      fontSize: {
+        base: '1rem',        // 16px (body)
+        'sm-detail': '0.875rem', // 14px
+        'h1': '2.25rem',     // 36px
+        'h2': '1.5rem',      // 24px
+        'h3': '1.25rem',     // 20px
       },
     },
   },
-  plugins: [require('tailwind-scrollbar-hide'),],
 };
