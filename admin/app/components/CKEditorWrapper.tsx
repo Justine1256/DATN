0965415addc9c76f44a1aaaa103a1847bea789c0
vannhatcr123 <vannhatcr2003@gmail.com>
2025-06-28 12:19:27 +0700

    a

--- admin/app/components/CKEditorWrapper.tsx
+++ admin/app/components/CKEditorWrapper.tsx
@@ -1,17 +1,10 @@
 "use client";
 import dynamic from "next/dynamic";
+import ClassicEditor from "@ckeditor/ckeditor5-build-classic";
 
-// Dynamic import CKEditor (React component)
 export const CKEditor = dynamic(
     () => import("@ckeditor/ckeditor5-react").then((mod) => mod.CKEditor),
     { ssr: false }
 );
 
-// Dynamic import cả ClassicEditor build để tránh lỗi window
-export const ClassicEditor = dynamic(
-    async () => {
-        const mod = await import("@ckeditor/ckeditor5-build-classic");
-        return mod.default;
-    },
-    { ssr: false }
-);
+export { ClassicEditor };
