681710683438b29bc0601701414453209c57fcd8
nhan502n <nhan502n@gmail.com>
2025-07-08 23:17:22 +0700

    fix

--- back-end/config/sanctum.php
+++ back-end/config/sanctum.php
@@ -15,7 +15,7 @@ return [
     |
     */
 
-    'stateful' => explode(',', env('SANCTUM_STATEFUL_DOMAINS', 'localhost:3000')),
+    'stateful' => explode(',', env('SANCTUM_STATEFUL_DOMAINS', 'localhost:3000,api.marketo.info.vn')),
 
 
     /*
@@ -30,7 +30,8 @@ return [
     |
     */
 
-    'guard' => ['web'],
+    'guard' => ['web', 'sanctum'],
+
 
     /*
     |--------------------------------------------------------------------------
