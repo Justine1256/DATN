c113a64ed1187c8043017371621b62e18d29efb7
zhen108 <baotruong1082003@gmail.com>
2025-07-18 03:05:56 +0700

    up

--- back-end/app/Http/Controllers/CartController.php
+++ back-end/app/Http/Controllers/CartController.php
@@ -117,7 +117,7 @@ public function store(Request $request)
             $cart = Cart::create([
                 'user_id'        => $userId,
                 'product_id'     => $product->id,
-                'variant_id'     => optional($variant)->id, // nullable
+                'variant_id' => $variantId, // nullable
                 'quantity'       => $quantity,
                 'product_option' => $productOption,
                 'product_value'  => $productValue,
