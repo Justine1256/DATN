836bc641c9b1ee8a6a2fdfedbd6f6f910be4dc8f
nhan502n <nhan502n@gmail.com>
2025-08-03 17:24:37 +0700

    a

--- back-end/app/Http/Controllers/UserController.php
+++ back-end/app/Http/Controllers/UserController.php
@@ -45,18 +45,20 @@ public function login(Request $request)
     $isLocalhost = in_array($host, ['localhost', '127.0.0.1']);
     $domain = $isLocalhost ? null : '.marketo.info.vn';
 
-    // Set cookie HttpOnly
-    $cookie = cookie(
-        'token',
-        $token,
-        60 * 24,             // 1 ngày
-        '/',
-        $domain,
-        !$isLocalhost,       // Secure: true nếu production
-        true,                // HttpOnly
-        false,               // Raw
-        'None'               // SameSite=None
-    );
+    $sameSite = $isLocalhost ? 'Lax' : 'None';
+
+$cookie = cookie(
+    'token',
+    $token,
+    60 * 24,
+    '/',
+    $domain,
+    !$isLocalhost,  // Secure = true nếu production
+    true,
+    false,
+    $sameSite
+);
+
 
     return response()->json([
         'user' => $user,
