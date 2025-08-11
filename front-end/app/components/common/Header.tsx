c0ccc98b96b78dc433e73ae3f82b3cbf09c3ad97
nhan502n <nhan502n@gmail.com>
2025-08-11 10:12:38 +0700

    sửa để code local

--- front-end/app/components/common/Header.tsx
+++ front-end/app/components/common/Header.tsx
@@ -542,16 +542,16 @@ const Header = () => {
 
                       {(user?.role === "admin" || user?.role === "seller") && (
                         <Link
-                          // href={
-                          //   user.role === "admin"
-                          //     ? "http://localhost:3001/admin/dashboard"
-                          //     : "http://localhost:3001/shop-admin/dashboard"
-                          // }
-                            href={
+                          href={
                             user.role === "admin"
-                              ? "https://admin.marketo.info.vn/admin/dashboard"
-                              : "https://admin.marketo.info.vn/shop-admin/dashboard"
+                              ? "http://localhost:3001/admin/dashboard"
+                              : "http://localhost:3001/shop-admin/dashboard"
                           }
+                          //   href={
+                          //   user.role === "admin"
+                          //     ? "https://admin.marketo.info.vn/admin/dashboard"
+                          //     : "https://admin.marketo.info.vn/shop-admin/dashboard"
+                          // }
                           
                           className="flex items-center gap-2 hover:bg-white/10 px-3 py-2 rounded"
                         >
