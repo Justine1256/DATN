e9429618cc1df117ff2931971779a3bf2574b0cb
vannhatcr123 <vannhatcr2003@gmail.com>
2025-08-18 09:48:13 +0700

    Update not-found.tsx

--- front-end/app/not-found.tsx
+++ front-end/app/not-found.tsx
@@ -5,9 +5,9 @@ import Link from 'next/link';
 export default function NotFound() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen text-center px-4 bg-white">
-      <h1 className="text-6xl font-bold text-black mb-4">404 Not Found</h1>
+      <h1 className="text-6xl font-bold text-black mb-4">404 Không tìm thấy</h1>
       <p className="text-base text-gray-600 mb-6">
-        The page you visited does not exist.
+        Trang bạn đã truy cập không tồn tại.
       </p>
 
       {/* ✅ Link back dùng prefetch & replace */}
@@ -17,7 +17,7 @@ export default function NotFound() {
         replace
         className="bg-red-500 text-white px-6 py-3 rounded-md hover:bg-red-600 transition"
       >
-        Back to home page
+       Trở về trang chủ
       </Link>
     </div>
   );
