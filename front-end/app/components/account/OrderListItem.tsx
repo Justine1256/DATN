bd1b4ec794f46e2c2f87c6cb471681b14946ea26
zhen108 <baotruong1082003@gmail.com>
2025-08-24 22:54:52 +0700

    Update OrderListItem.tsx

--- front-end/app/components/account/OrderListItem.tsx
+++ front-end/app/components/account/OrderListItem.tsx
@@ -505,14 +505,6 @@ export default function OrderListItem({
                                     Tố cáo shop
                                 </button>
                             )}
-
-
-
-
-
-
-
-
                         {order.order_status === "Canceled" && (
                             <button
                                 className="inline-flex items-center gap-2 px-6 py-3 bg-gradient-to-r from-blue-500 to-indigo-500 text-white rounded-xl hover:from-blue-600 hover:to-indigo-600 transition-all duration-200 font-semibold shadow-lg shadow-blue-500/20"
